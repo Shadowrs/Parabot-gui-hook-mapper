3661664886e732884839a4802af2f2c8c156ceb5
Jak <shadow_jak@hotmail.co.uk>
2018-07-23 12:51:18 +0100

    Add Cell Factory to render List items as entry.className instead of toString()

--- src/com/jackw/Controller.java
+++ src/com/jackw/Controller.java
@@ -1,7 +1,9 @@
 package com.jackw;
 
 import com.jackw.logic.ApiData;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
@@ -9,6 +11,7 @@ import javafx.event.EventTarget;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
 import org.controlsfx.control.Notifications;
 
 public class Controller {
@@ -16,7 +19,7 @@ public class Controller {
 	@FXML private Label lblApiPath;
 	@FXML private Button btnLoadApiJar;
 	@FXML private Button btnReloadApiJar;
-	@FXML private ListView<ApiData.ApiInterface> list1;
+	@FXML private ListView<ListEntryAdapter<ApiData.ApiInterface>> list1;
 	@FXML private ListView<ApiData.ClientEntry> list2;
 	@FXML private TableView<PbLink> table1;
 	@FXML MenuItem menuitem_link_api_interface;
@@ -33,7 +36,10 @@ public class Controller {
 		if (target == btnLoadApiJar) {
 			data = new ApiData();
 			lblApiPath.setText(data.pbApi.apiJar.getAbsolutePath());
-			list1.setItems(FXCollections.observableArrayList(data.pbApi.interfaces));
+			List<ListEntryAdapter<ApiData.ApiInterface>> list = data.pbApi.interfaces.stream()
+					.map(i -> new ListEntryAdapter<>(i.getClass().getName(), i))
+					.collect(Collectors.toList());
+			list1.setItems(FXCollections.observableArrayList(list));
 			list2.setItems(FXCollections.observableArrayList(data.client.entries));
 			Notifications.create().title("Parabot Mapper").text("Loaded API JAR").show();
 		}
@@ -55,7 +61,7 @@ public class Controller {
 	}
 
 	private void link() {
-		ApiData.ApiInterface x = list1.getSelectionModel().getSelectedItem();
+		ListEntryAdapter<ApiData.ApiInterface> x = list1.getSelectionModel().getSelectedItem();
 		ApiData.ClientEntry c = list2.getSelectionModel().getSelectedItem();
 		if (x == null || c == null) {
 			System.out.println("bad selection");
@@ -97,6 +103,50 @@ public class Controller {
 		System.out.println("initialzing!");
 		apiClass.setCellValueFactory(new PropertyValueFactory<>("apiClass"));
 		clientClass.setCellValueFactory(new PropertyValueFactory<>("clientClass"));
+		list1.setCellFactory(new Callback<>() {
+			@Override
+			public ListCell<ListEntryAdapter<ApiData.ApiInterface>> call(ListView<ListEntryAdapter<ApiData.ApiInterface>> param) {
+				return new ListCell<>() {
+					@Override
+					protected void updateItem(ListEntryAdapter<ApiData.ApiInterface> item, boolean empty) {
+						if (item == null) return;
+						super.updateItem(item, empty);
+						setText(item.getO().getClass().getSimpleName());
+					}
+				};
+			}
+		});
 		System.out.println("ready");
+	}
+
+	public class ListEntryAdapter<T> {
+		private final SimpleStringProperty e; // TODO could actually just be a string i think
+		// no need for this wrapper
+		private T o;
+
+		ListEntryAdapter(String display, T o) {
+			e = new SimpleStringProperty(display);
+			this.o = o;
+		}
+
+		public String getE() {
+			return e.get();
+		}
+
+		public SimpleStringProperty eProperty() {
+			return e;
+		}
+
+		public void setE(String e) {
+			this.e.set(e);
+		}
+
+		public T getO() {
+			return o;
+		}
+
+		public void setO(T o) {
+			this.o = o;
+		}
 	}
 }
